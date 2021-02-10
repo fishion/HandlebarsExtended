9be67e9b153bf2a9a34d4a58a497bb55a514872e
Alex <alex@monney.me>
2021-02-10 09:33:16 +0000

    destructured params and defaults. Use path lib

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 "use strict";
 
-const Handlebars = require('handlebars');
 const fs = require('fs');
+const path = require('path')
+const Handlebars = require('handlebars');
 
-const wrappersPath = "partials/wrappers";
-const includesPath = "partials/includes";
-
-module.exports = (templatesDir, renderedDir) => {
+module.exports = ({
+  appRoot,
+  outputPath     = 'docs',
+  controllerPath = 'controllers',
+  pagesPath      = 'templates/pages',
+  wrappersPath   = "templates/partials/wrappers",
+  includesPath   = "templates/partials/includes",
+}) => {
   const hbs = Handlebars
 
   // add helpers
   hbs.registerHelper("wrap", function(wrapper, options) {
     // if performance was a consideration, may be worth pre-compiling and caching wrappers
     const compiledWrapper = hbs.compile(
-      fs.readFileSync(`${templatesDir}/${wrappersPath}/${wrapper}.html.hbs`,'utf8').toString()
+      fs.readFileSync(`${path.join(appRoot, wrappersPath, wrapper)}.html.hbs`,'utf8').toString()
     );
     return compiledWrapper({ ...this, ...options.hash, content : options.fn(this) })
   });
@@ -21,7 +26,7 @@ module.exports = (templatesDir, renderedDir) => {
   hbs.registerHelper("include", function(include, options) {
     // if performance was a consideration, may be worth pre-compiling and caching includes
     const compiledInclude = hbs.compile(
-      fs.readFileSync(`${templatesDir}/${includesPath}/${include}.html.hbs`,'utf8').toString()
+      fs.readFileSync(`${path.join(appRoot, includesPath, include)}.html.hbs`,'utf8').toString()
     );
     return compiledInclude({ ...this, ...options.hash})
   });
@@ -39,24 +44,36 @@ module.exports = (templatesDir, renderedDir) => {
   });
 
 
-  // main render method
-  hbs.render = (templateName) => {
-    // pull apart template path
-    const [fileName, fileExt] = templateName.split('.'); // myfile.html.hbs
-    console.log(`Rendering ${fileName}`)
+  // render to a file
+  hbs.render = (templateName, templateParams) => {
+    // pull apart template path e.g. myfile.html.hbs
+    const outFile = path.basename(templateName, '.hbs');
+    const fileObj = path.parse(outFile) 
+    console.log(`Rendering ${fileObj.name}`)
 
     // pull data for each page, if there is any needed
     try {
-      var pageData = require(`../controllers/${fileName}`);
-    } catch (e) {console.log(`  - no data for ${fileName}`)}
+      var pageData = require(path.join(controllerPath, fileObj.name));
+    } catch (e) {console.log(`  - no data for ${fileObj.name}`)}
 
     // render template
-    const template = hbs.compile( fs.readFileSync(`${templatesDir}/${templateName}`,'utf8').toString() );
-    fs.writeFileSync(`${renderedDir}/${fileName}.${fileExt}`, template({
-      pagename : { [fileName] : true },
+    const template = hbs.compile( fs.readFileSync(path.join(appRoot, pagesPath, templateName),'utf8').toString() );
+    fs.writeFileSync(path.join(appRoot, outputPath, outFile), template({
+      pagename : {
+        name : fileObj.name,
+        is : { [fileObj.name] : true },
+      },
+      ...templateParams,
       ...pageData
     }))
-    console.log(`  - finished rendering ${fileName}`)
+    console.log(`  - finished rendering ${fileObj.name}`)
+  }
+
+  hbs.buildSite = (templateParams) => {
+    // read file lists & render
+    fs.readdirSync(path.join(appRoot, pagesPath), {withFileTypes: true})
+      .filter(item => !item.isDirectory())
+      .forEach(file => hbs.render(file.name, templateParams))
   }
 
   return hbs;
