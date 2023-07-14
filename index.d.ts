2eb91b1fe07f5c496faa3a0ace0030098a11dbc0
Alex <alex@monney.me>
2023-07-15 00:13:40 +0100

    add ts type definitions

---
+++ index.d.ts
@@ -0,0 +1,12 @@
+declare function _exports({ appRoot, outputPath, controllerPath, pagesPath, wrappersPath, includesPath, extension }: {
+    appRoot: any;
+    outputPath?: string;
+    controllerPath?: string;
+    pagesPath?: string;
+    wrappersPath?: string;
+    includesPath?: string;
+    extension?: string;
+}): typeof Handlebars;
+export = _exports;
+import Handlebars = require("handlebars");
+//# sourceMappingURL=index.d.ts.map
